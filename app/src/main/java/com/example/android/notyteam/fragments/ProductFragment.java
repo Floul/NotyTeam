1c2902123894edecd514ee26095e0e75cd248328
Oleksandr Nikulochkin <i.am.floul@gmail.com>
2019-09-08 01:35:25 +0300

    First commit

--- app/src/main/java/com/example/android/notyteam/fragments/ProductFragment.java
+++ app/src/main/java/com/example/android/notyteam/fragments/ProductFragment.java
@@ -6,25 +6,18 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.notyteam.adapters.DetailsTabAdapter;
 import com.example.android.notyteam.adapters.ImagesPagerAdapter;
 import com.example.android.notyteam.parsingdata.Product;
 import com.example.android.notyteam.R;
-import com.example.android.notyteam.parsingdata.SingleProductData;
-import com.example.android.notyteam.api.WebApi;
-import com.google.android.material.tabs.TabLayout;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
@@ -32,13 +25,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class ProductFragment extends Fragment {
 
-    ImageView productImage;
-    TextView productPrice;
-    TextView productName;
-    ViewPager imagePager;
-    ImagesPagerAdapter imagesPagerAdapter;
-    ViewPager detailsPager;
-    DetailsTabAdapter detailsAdapter;
+    private ImageView productImage;
+    private TextView productPrice;
+    private TextView productName;
+    public TextView imagePageCount;
+    private ViewPager imagePager;
+    private ImagesPagerAdapter imagesPagerAdapter;
+    private ViewPager detailsPager;
+    private DetailsTabAdapter detailsAdapter;
 
     private int productId;
     private Product product;
@@ -48,17 +42,34 @@ public class ProductFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_product, container, false);
         if (getArguments() != null) {
-            product =(Product) getArguments().getSerializable("product");
+            product = (Product) getArguments().getSerializable("product");
             productId = getArguments().getInt("productId");
         }
         productImage = view.findViewById(R.id.product_image);
         productPrice = view.findViewById(R.id.product_price_tv);
         productName = view.findViewById(R.id.product_name_tv);
+        imagePageCount = view.findViewById(R.id.page_count);
         imagePager = view.findViewById(R.id.pager);
         detailsPager = view.findViewById(R.id.details_pager);
 
         imagesPagerAdapter = new ImagesPagerAdapter(getFragmentManager(), product);
         imagePager.setAdapter(imagesPagerAdapter);
+        imagePager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                imagePageCount.setText(pageCountFormater(position));
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         detailsAdapter = new DetailsTabAdapter(getFragmentManager(), product);
         detailsPager.setAdapter(detailsAdapter);
 
@@ -67,8 +78,15 @@ public class ProductFragment extends Fragment {
     }
 
     private void setUpUi() {
-        productPrice.setText(Integer.toString(product.getPrice()) + " руб.");
+        productPrice.setText(product.getPrice() + " руб.");
         productName.setText(product.getName());
     }
+
+    private String pageCountFormater(int position){
+        String pages;
+            pages = position+1 + "/" +  product.getImages().size();
+        return pages;
+    }
+
 
 }
